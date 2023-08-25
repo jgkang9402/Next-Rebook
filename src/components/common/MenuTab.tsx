74be040011f086c4f52e37d054e7aceae006278e
jg <jgkang9402@gmail.com>
2023-08-25 12:35:36 +0900

    Feat: 공통 메뉴탭 컴포넌트 생성

--- src/components/common/MenuTab.tsx
+++ src/components/common/MenuTab.tsx
@@ -1,35 +1,28 @@
-"use client";
-
-import { useRouter, useSearchParams } from "next/navigation";
-import React from "react";
-
 export interface tabType {
-  labelName: string;
-  pathType: string;
+  menuTitle: string;
+  menuType: string;
 }
-
 interface MenuTabProps {
   tabList: tabType[];
+  currentMenu: string;
+  handleCurrentMenu: (menu: string) => void;
 }
-const MenuTab = ({ tabList }: MenuTabProps) => {
-  const router = useRouter();
-  const search = useSearchParams();
-
+const MenuTab = ({ tabList, currentMenu, handleCurrentMenu }: MenuTabProps) => {
   return (
     <div className="bg-white">
       <ul className="flex">
         {tabList.map((item: tabType) => {
           return (
-            <li key={item.pathType} className="w-full flex-1 block">
+            <li key={item.menuType} className="w-full flex-1 block">
               <button
-                onClick={() => router.push(`?curtab=${item.pathType}`)}
+                onClick={() => handleCurrentMenu(item.menuType)}
                 className={`w-full py-4 px-6 block hover:text-primary hover:border-primary focus:outline-none ${
-                  search.get("curtab") === item.pathType
+                  currentMenu === item.menuType
                     ? "text-primary border-primary"
                     : "text-gray-600"
                 } border-b-2 font-medium`}
               >
-                {item.labelName}
+                {item.menuTitle}
               </button>
             </li>
           );
