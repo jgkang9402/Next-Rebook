5e32c4299df44e02248ecf5e27c13e405725b93d
jg <jgkang9402@gmail.com>
2023-08-26 02:24:56 +0900

    Feat: 공통 스피너 생성

---
+++ src/components/common/Spinner.tsx
@@ -0,0 +1,9 @@
+const Spinner = () => {
+  return (
+    <div className="flex items-center justify-center h-[60vh]">
+      <div className="animate-spin rounded-full h-32 w-32 border-t-8 border-b-8 border-primary" />
+    </div>
+  );
+};
+
+export default Spinner;
