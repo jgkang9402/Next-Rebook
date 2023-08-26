055cfef4acfc6649565f22ebf25e2487e7d53149
jg <jgkang9402@gmail.com>
2023-08-26 16:52:02 +0900

    Style: 스피너컴포넌트 spinnerBoxHeight 프롭스추가

--- src/components/common/Spinner.tsx
+++ src/components/common/Spinner.tsx
@@ -1,6 +1,8 @@
-const Spinner = () => {
+const Spinner = ({ spinnerBoxHeight }: { spinnerBoxHeight?: string }) => {
   return (
-    <div className="flex items-center justify-center h-[60vh]">
+    <div
+      className={`flex items-center justify-center my-auto ${spinnerBoxHeight}`}
+    >
       <div className="animate-spin rounded-full h-32 w-32 border-t-8 border-b-8 border-primary" />
     </div>
   );
